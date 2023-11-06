20053b2b9be55f471a443ff9fd77006774226b84
adunkellies <adunkellies@gmail.com>
2023-11-06 12:48:00 +0900

    rpa_project finished

--- rpa_project/01_send_test_mail.py
+++ rpa_project/01_send_test_mail.py
@@ -3,6 +3,27 @@
 # 본문 : 닉네임/전화번호 뒤 4자리 (Random)
 #   (예) 나도코딩/1234
 
+from random import *
+
 import smtplib
 from account import *
 from email.message import EmailMessage
+
+nicknames = ["유재석", "박명수","정형돈", "노홍철", "조세호"]
+
+with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    for nickname in nicknames:
+        msg = EmailMessage()
+        msg["Subject"] = "파이썬 특강 신청합니다."
+        msg["From"] = EMAIL_ADDRESS
+        msg["To"] = "adunkellies@gmail.com"
+        
+        # content = nickname + "/" + str(randint(1000, 9999))
+        content = "/".join([nickname, str(randint(1000, 9999))])
+        msg.set_content(content)
+        smtp.send_message(msg)
+        print(nickname + "님이 내 계정으로 메일 발송 완료")
